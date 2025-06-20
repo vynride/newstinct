88eebee4bfe8d816357d691c3ea7dfc006ea1b35
Vivian <vynride@gmail.com>
2025-06-20 13:49:32 +0530

    add basic design

---
+++ src/components/NewsContainer.tsx
@@ -0,0 +1,33 @@
+import { promises as fs } from "fs";
+import { ReactNode } from "react";
+import path from "path";
+
+export async function NewsContainer(): Promise<ReactNode> {
+  const file: any = await fs.readFile(
+    path.join(process.cwd(), "public/articles/articles.json"),
+    "utf-8"
+  );
+  const data = JSON.parse(file);
+
+  const renderList = (): ReactNode => {
+    return (
+      <ol className="text-center w-full mx-5">
+        {/* TODO :: Define type for this JSON item bro */}
+        {data.map((item: any, index: number) => (
+          <li
+            className="bg-gray-900 text-white mb-3 py-3 px-5 text-md rounded-3xl text-wrap:balance"
+            key={index}
+          >
+            {item.headline}
+          </li>
+        ))}
+      </ol>
+    );
+  };
+
+  return (
+    <div className=" bg-gray-950 continer flex justify-center items-center mt-15 mx-5 py-10 rounded-4xl">
+      {renderList()}
+    </div>
+  );
+}
